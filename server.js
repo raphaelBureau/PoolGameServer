ab758846c116732280e7cedc9972381e7cf5baa0
raphaelBureau <raphael.burea@gmail.com>
2023-10-13 21:29:27 -0400

    https

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const { Server } = require("socket.io");
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+
+
+const httpServer = createServer({
+    key: readFileSync("/etc/letsencrypt/live/bureau.blue/privkey.pem"),
+    cert: readFileSync("/etc/letsencrypt/live/bureau.blue/fullchain.pem")
+});
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
 
 var games = {}; //jsp si cest une bonne idée mais javascript cest magique
 games["pool"] = {}; //list de pool
 games["pool"]["waiting"] = [] //array de socket.id pour l'instant ya surement une meuilleure maniere
 games["pool"]["playing"] = [] //dictionaire ou [(player1) socket.id] = (player2) socket.id et [(player2) socket.id] = (player1) socket.id  encore, il y a surement une meuilleure maniere
 
-const io = new Server({
-    cors: {
-        origin: "*"
-    }
-});
-
 io.on("connection", (socket) => {
 
     socket.on("disconnect", (reason) => {
-        if(games["pool"]["waiting"].includes(socket.id)) {
+        if (games["pool"]["waiting"].includes(socket.id)) {
 
-            games["pool"]["waiting"].splice(games["pool"]["waiting"].indexOf(socket.id),1);
+            games["pool"]["waiting"].splice(games["pool"]["waiting"].indexOf(socket.id), 1);
             console.log("waiting player removed : " + socket.id);
-        } 
-        if(games["pool"]["playing"].hasOwnProperty(socket.id)){
+        }
+        if (games["pool"]["playing"].hasOwnProperty(socket.id)) {
 
             let id = games["pool"]["playing"][socket.id];
 
@@ -30,14 +38,14 @@ io.on("connection", (socket) => {
 
             console.log("Left game, player: " + socket.id + " , disconnecting: " + id);
         }
-      });
+    });
 
     socket.on("joinGame", (arg) => {
-        if(games["pool"]["waiting"].length >= 1) {
+        if (games["pool"]["waiting"].length >= 1) {
 
             let p2Id = games["pool"]["waiting"][0];
 
-            games["pool"]["playing"][ p2Id ] = socket.id; //player 1
+            games["pool"]["playing"][p2Id] = socket.id; //player 1
             games["pool"]["playing"][socket.id] = p2Id; //player 2
 
             socket.to(p2Id).emit("gameStart", "true"); //player1
@@ -45,9 +53,9 @@ io.on("connection", (socket) => {
 
             console.log("Match Found p1: " + p2Id + " p2: " + socket.id);
 
-            games["pool"]["waiting"].splice(0,1); //possiblement vraiment dangereux
+            games["pool"]["waiting"].splice(0, 1); //possiblement vraiment dangereux
         }
-        else{
+        else {
             games["pool"]["waiting"].push(socket.id); //waiting for players
 
             console.log("Waiting: " + socket.id);
@@ -69,4 +77,4 @@ io.on("connection", (socket) => {
     });
 });
 
-io.listen(3000);
+httpServer.listen(3000);
