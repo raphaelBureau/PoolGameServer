38e3ad7a483552fd1404e6f5a9d99feab6e398b0
raphaelBureau <94479381+raphaelBureau@users.noreply.github.com>
2023-11-28 22:45:51 -0500

    private lobbies

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.log("starting server");
 const { Server } = require("socket.io");
 const { readFileSync } = require("fs");
 const { createServer } = require("https");
@@ -16,10 +17,9 @@ const io = new Server(httpServer, {
 
 var games = {}; //jsp si cest une bonne idée mais javascript cest magique
 games["pool"] = {}; //list de pool
-games["pool"]["waiting"] = [] //array de socket.id pour l'instant ya surement une meuilleure maniere
 games["pool"]["playing"] = [] //dictionaire ou [(player1) socket.id] = (player2) socket.id et [(player2) socket.id] = (player1) socket.id  encore, il y a surement une meuilleure maniere
 
-var privateQueue = []; //partie privee
+var queue = []; //lobbys en attente d'un autre joueur
 
 var playerInfoList = []; //contient les infos des joueurs {name:"nom",profile:"url"}
 
@@ -27,10 +27,11 @@ io.on("connection", (socket) => {
     socket.emit("connected", "args");
 
     socket.on("disconnect", (reason) => {
-        if (games["pool"]["waiting"].includes(socket.id)) {
+        if (queue.forEach((id) => {if(id == socket.id){
+            return true;
+        }})) {
 
-            games["pool"]["waiting"].splice(games["pool"]["waiting"].indexOf(socket.id), 1);
-            console.log("waiting player removed : " + socket.id);
+            delete queue[queue.indexOf(socket.id)];
         }
         if (games["pool"]["playing"].hasOwnProperty(socket.id)) {
 
@@ -47,45 +48,36 @@ io.on("connection", (socket) => {
 
     socket.on("joinGame", (userInfo) => {
         //validation
+        if(/^[\[|\{](\s|.*|\w)*[\]|\}]$/.test(userInfo))  {
         console.log(userInfo);
         userInfo = JSON.parse(userInfo);
         if(userInfo.hasOwnProperty("id") && userInfo.hasOwnProperty("gamertag") && userInfo.hasOwnProperty("img")) {
-         //   let profileData = {};
-         //   profileData.lobbyID = userInfo.lobbyID;
-         //   if(userInfo.token == "none") {
-        //    console.log("guest user join request id:" + socket.id + ", GuestName: " + userInfo.id + ", token: " + userInfo.token + ", lobbyID:" + userInfo.lobbyID);
-        //    profileData.name = userInfo.id;
-       //     }
-       //     else{
-                //a faire plus tard authentification
-          //  console.log("registered user join request id:" + socket.id + ", userID: " + userInfo.id + ", token: " + userInfo.token + ", lobbyID:" + userInfo.lobbyID);
-        //    profileData.name = 
-       //     }
+            if(!userInfo.hasOwnProperty("lobby")) {
+                userInfo.lobby = "";
+            }
+            playerInfoList[socket.id] = JSON.stringify(userInfo);
+        if (queue[userInfo.lobby] != undefined) {
 
-       playerInfoList[socket.id] = JSON.stringify(userInfo);
-
-        if (games["pool"]["waiting"].length >= 1) {
-
-            let p2Id = games["pool"]["waiting"][0];
+            let p2Id = queue[userInfo.lobby];
 
             games["pool"]["playing"][p2Id] = socket.id; //player 1
             games["pool"]["playing"][socket.id] = p2Id; //player 2
 
-            socket.to(p2Id).emit("opponentInfo", playerInfoList[socket.id])//player1
-            socket.to(p2Id).emit("gameStart", "true"); //player1
-            socket.emit("opponentInfo", playerInfoList[p2Id])//player2
-            socket.emit("gameStart", "false"); //player2
+            socket.to(p2Id).emit("gameStart", "1" + playerInfoList[socket.id]); //player1
+            socket.emit("gameStart", "0" + playerInfoList[p2Id]); //player2
 
             console.log("Match Found p1: " + p2Id + " p2: " + socket.id);
-
-            games["pool"]["waiting"].splice(0, 1); //possiblement vraiment dangereux
+            delete queue[userInfo.lobby];
         }
         else {
-            games["pool"]["waiting"].push(socket.id); //waiting for players
-
-            console.log("Waiting: " + socket.id);
+            queue[userInfo.lobby] = socket.id; //waiting for players
+            console.log("Waiting: " + socket.id + ", custom lobby: " + '"' +userInfo.lobby + '"' + ", total waiting: " + Object.keys(queue).length + ", total playing: " + (Object.keys(queue).length + Object.keys(games["pool"]["playing"]).length));
         }
     }
+}
+else{
+    console.log("invalid playerInfo from: " + socket.id + " info: " + userInfo)
+}
     })
     socket.on("sendPos", (arg) => {
         socket.to(games["pool"]["playing"][socket.id]).volatile.emit("positions", arg);
@@ -132,3 +124,4 @@ io.on("connection", (socket) => {
 });
 
 httpServer.listen(3000);
+console.log("listening");
